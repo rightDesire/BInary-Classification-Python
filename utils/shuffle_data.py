b126c5e5cb869a7071fe66a418022a3d7e0f829a
rightDesire <14beav20@gmail.com>
2024-09-28 15:38:39 +0300

    Распределяет зону ответственности функции prepare_data на shuffle_data и split_data

---
+++ utils/shuffle_data.py
@@ -0,0 +1,17 @@
+﻿import numpy as np
+
+
+def shuffle_data(data):
+    """
+    Перемешивает данные.
+
+    Parameters:
+    data (numpy array): Исходные данные
+
+    Returns:
+    numpy array: Перемешанные данные
+    """
+    # Перемешивание данных
+    random_indices = np.random.permutation(data.shape[0])
+    shuffled_data = data[random_indices]
+    return shuffled_data
---
+++ utils/split_data.py
@@ -0,0 +1,26 @@
+﻿import numpy as np
+
+
+def split_data(shuffled_data, train_ratio=0.7):
+    """
+    Делит данные на обучающую и тестовую выборки.
+
+    Parameters:
+    shuffled_data (numpy array): Перемешанные данные
+    train_ratio (float): Процент данных для обучения (по умолчанию 70%)
+
+    Returns:
+    X_train, y_train, X_test, y_test: Разделенные данные
+    """
+    # Размеры выборок
+    m_all = shuffled_data.shape[0]
+    m_training = int(np.round(m_all * train_ratio))  # 70% на обучение
+
+    # Разделение перемешанных данных на обучающую и тестовую выборки
+    X_train = shuffled_data[:m_training, :2]
+    y_train = shuffled_data[:m_training, 2]
+
+    X_test = shuffled_data[m_training:, :2]
+    y_test = shuffled_data[m_training:, 2]
+
+    return X_train, y_train, X_test, y_test
